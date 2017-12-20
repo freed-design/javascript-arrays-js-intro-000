f17dcf9485761f93c0be6f8c89ac42e9ad412cbb
Daniel Dunbar <danielqdunbar@gmail.com>
2017-12-20 04:07:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,6 +23,6 @@ var accessElementInArray = (arrayArgument, indexNumber) => {
 }
 
 var destructivelyRemoveElementFromBeginningOfArray = (arrayArgument) => {
-  return arrayArgument.shift()
-//  return arrayArgument
+  arrayArgument.shift()
+  return arrayArgument
 }
