641c971c72f016b32a6d00b04bfaaa2a4ee167c8
Daniel Dunbar <danielqdunbar@gmail.com>
2017-12-20 03:33:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,6 @@ var addElementToBeginningOfArray = (array, element) => {
   return [element, ...array]
 }
 
-var destructivelyAddElementToBeginningOfArray = (array, element) => {
-  return array.unshift(element)
-}
+//var destructivelyAddElementToBeginningOfArray = (array, element) => {
+//  return array.unshift(element)
+//}
